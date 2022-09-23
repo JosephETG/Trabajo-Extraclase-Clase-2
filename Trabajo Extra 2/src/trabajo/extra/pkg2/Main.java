a6730ce3a186b2f0b0a097889044876e86e2d6b9
JosephETG <98758695+JosephETG@users.noreply.github.com>
2022-09-23 02:40:50 -0600

    Add files via upload

---
+++ Trabajo Extra 2/src/trabajo/extra/pkg2/Main.java
@@ -0,0 +1,59 @@
+package trabajo.extra.pkg2;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        float HorasLaboradas;
+        float PrecioHora;
+        byte AñosAntiguedad;
+        double SalarioBruto;
+        double SalarioNeto;
+        double Deducciones;
+        double Bono;
+
+        AñosAntiguedad = Byte.parseByte(JOptionPane.showInputDialog(
+                "Ingrese su antiguedad en la empresa: "));
+
+        HorasLaboradas = Float.parseFloat(JOptionPane.showInputDialog(
+                "Ingrese la cantidad de horas laboradas a la semana: "));
+
+        PrecioHora = Float.parseFloat(JOptionPane.showInputDialog(
+                "Ingrese el valor de su hora: "));
+
+        SalarioBruto = ( HorasLaboradas * PrecioHora ) * 4;
+        
+        if ( AñosAntiguedad > 10 ) {
+
+            Bono = SalarioBruto * 0.20;
+
+            SalarioBruto = Bono + SalarioBruto;
+
+            if ( SalarioBruto > 1000 && SalarioBruto < 2000 ) {
+
+                Deducciones = SalarioBruto * 0.10;
+
+                SalarioNeto = SalarioBruto - Deducciones;
+
+                JOptionPane.showMessageDialog(null,
+                        "Su salario neto es: " + SalarioNeto);
+
+            } else if ( SalarioBruto > 2000 ) {
+
+                Deducciones = SalarioBruto * 0.15;
+
+                SalarioNeto = SalarioBruto - Deducciones;
+
+                JOptionPane.showMessageDialog(null,
+                        "Su salario neto es: " + SalarioNeto);
+            }
+        } else {
+
+            JOptionPane.showMessageDialog(null,
+                    "Su Salario es: " + SalarioBruto);
+        }
+    }
+    
+}
